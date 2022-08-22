2c936dc42e145fedf15ee774430ee3daffc6ff30
Tydomx <michaelthai16@gmail.com>
2022-08-22 18:42:07 -0500

    Added mutations, queries, apollo provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Apollo Provider/client
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// establishing new link to GraphQL server at /graphql endpoint with createHttpLink()
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// middleware function that retrieves token for us and combines it with existing httpLink
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
+// use ApolloClient constructor to instantiate Apollo Client instance and create connection to API endpoint
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,77 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+	mutation loginUser($email: String!, $password: String!) {
+		loginUser(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $password: String!, $email: String!) {
+		addUser(username: $username, password: $password, email: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const SAVE_BOOK = gql`
+	mutation saveBook(
+		$authors: [String], 
+		$description: String,
+		$bookId: ID,
+		$image: String,
+		$link: String,
+		$title: String) {
+			saveBook(
+				authors: $authors, 
+				description: $description,
+				bookId: $bookId,
+				image: $image,
+				link: $link,
+				title: $title) {
+					_id
+					username
+					email
+					password
+					bookCount
+					savedBooks {
+						authors
+						description
+						bookId
+						image
+						link
+						title
+					}
+				})
+		})
+`;
+
+export const REMOVE_BOOK = gql`
+		mutation removeBook($bookId: ID!) {
+			removeBook(bookId: $bookId) {
+				_id
+				username
+				email
+				password
+				bookCount
+				savedBooks {
+					authors
+					description
+					bookId
+					image
+					link
+					title
+				}
+			}
+		}
+`;
